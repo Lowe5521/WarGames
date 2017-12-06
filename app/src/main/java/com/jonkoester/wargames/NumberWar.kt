3cc7058a917ae911a78562139824e549566ae0ec
Jonathan Koester <Jonathan.D.Koester@ehi.com>
2017-12-06 08:36:36 -0600

    Initial commit

---
+++ app/src/main/java/com/jonkoester/wargames/NumberWar.kt
@@ -0,0 +1,12 @@
+package com.jonkoester.wargames
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class NumberWar : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_number_war)
+    }
+}
