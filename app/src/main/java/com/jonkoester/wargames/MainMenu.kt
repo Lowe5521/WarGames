1ceb81b2775944d6e743e7fd371f87bf27a2d7e0
Jonathan Koester <Jonathan.D.Koester@ehi.com>
2017-12-07 10:17:00 -0600

    - implemented the Kotterknife library - implemented the main menu screen v0.1 with button functionality

--- app/src/main/java/com/jonkoester/wargames/MainMenu.kt
+++ app/src/main/java/com/jonkoester/wargames/MainMenu.kt
@@ -1,11 +1,22 @@
 package com.jonkoester.wargames
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import kotterknife.bindView
 
 class MainMenu : AppCompatActivity() {
+
+    val btnNumberWar: Button by bindView<Button>(R.id.number_war_btn)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+        btnNumberWar.setOnClickListener {
+            val intent = Intent(this, NumberWar::class.java)
+            startActivity(intent)
+        }
     }
 }
